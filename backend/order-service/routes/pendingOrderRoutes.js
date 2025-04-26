567073b5e4c2bbd0c2029e00642792adbc989f35
sathu0622 <sathushan622@gmail.com>
2025-04-26 12:57:05 +0530

    status

--- backend/order-service/routes/pendingOrderRoutes.js
+++ backend/order-service/routes/pendingOrderRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 
-const { createPendingOrder } = require("../controllers/pendingOrderController");
+const { createPendingOrder, updatePendingOrderStatus } = require("../controllers/pendingOrderController");
 
 
 router.post("/pending-order", createPendingOrder);
+router.patch("/pending-orders/:id/status", updatePendingOrderStatus);
 
 module.exports = router;
